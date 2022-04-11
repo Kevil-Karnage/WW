4d7acdc3ddbfc01f031ebb22f42c224aee6eec41
Kevil-Karnage <69972754+Kevil-Karnage@users.noreply.github.com>
2022-04-11 16:28:40 +0300

    add base for application

---
+++ WhoWins/src/main/java/com/program/WhoWins/WhoWinsApplication.java
@@ -0,0 +1,13 @@
+package com.program.WhoWins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoWinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoWinsApplication.class, args);
+	}
+
+}
---
+++ WhoWins/src/test/java/com/program/WhoWins/WhoWinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.WhoWins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoWinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
